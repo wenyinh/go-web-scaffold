ca55c1730dcc60f79b4223f49e67b9329aeddd49
闻胤 <hewenyin.hwy@alibaba-inc.com>
2025-06-09 19:26:06 +0800

    update gorm test

--- logic/verify/mysql_verify.go
+++ logic/verify/mysql_verify.go
@@ -7,6 +7,7 @@ import (
 	"go-web-scaffold/dao/mysql"
 	"go-web-scaffold/models"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 func createTableIfNotExists() (err error) {
@@ -36,12 +37,72 @@ func createUser(name, email string) (err error) {
 
 func getUserByName(name string) (*models.User, error) {
 	var user models.User
-	if err := mysql.DB.Model(&user).Where("name = ?", name).First(&user).Error; err != nil {
+	if err := mysql.DB.Model(&models.User{}).Where("name = ?", name).First(&user).Error; err != nil {
 		zap.L().Error("get user failed", zap.Error(err))
 		return nil, err
 	}
 	fmt.Println("get user success")
 	return &user, nil
+}
+
+func getGroupsOfUserByIdRange(between, end int64) (err error) {
+	var users []models.User
+	res := mysql.DB.Model(&models.User{}).Where("id between ? and ?", between, end).Find(&users)
+	err = res.Error
+	if err != nil {
+		zap.L().Error("get users failed", zap.Error(res.Error))
+		return res.Error
+	}
+	fmt.Println("get users success, rows affected:", res.RowsAffected)
+	return
+}
+
+func updateUser(name, email string) (err error) {
+	if err = createTableIfNotExists(); err != nil {
+		zap.L().Error("create table failed", zap.Error(err))
+		return
+	}
+	err = mysql.DB.Model(&models.User{}).Where("name = ?", name).Update("email", email).Error
+	if err != nil {
+		zap.L().Error("update user failed", zap.Error(err))
+		return
+	}
+	fmt.Println("update user success")
+	return
+}
+
+func saveUser(name, email string, id uint) (err error) {
+	if err = createTableIfNotExists(); err != nil {
+		zap.L().Error("create table failed", zap.Error(err))
+		return
+	}
+	user := models.User{Model: gorm.Model{ID: id}, Name: name, Email: email}
+	res := mysql.DB.Model(&models.User{}).Save(&user)
+	if res.Error != nil {
+		zap.L().Error("save user failed", zap.Error(res.Error))
+		return
+	}
+	fmt.Println("save user success")
+	return
+}
+
+func deleteUserById(id int64) (err error) {
+	if err = createTableIfNotExists(); err != nil {
+		zap.L().Error("create table failed", zap.Error(err))
+		return
+	}
+	res := mysql.DB.Model(&models.User{}).Where("id = ?", id).Or("name = ?", "HWY").Delete(&models.User{})
+	if res.Error != nil {
+		zap.L().Error("delete user failed", zap.Error(res.Error))
+		return
+	}
+	if res.RowsAffected == 0 {
+		err = errors.New("delete user failed, no rows affected")
+		zap.L().Error("delete user failed", zap.Error(err))
+		return
+	}
+	fmt.Println("delete user success")
+	return
 }
 
 func TestMySQL() error {
