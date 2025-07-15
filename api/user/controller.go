df355e27dabfa4a601625e840645587b42d164d1
wenyinh <wenyinh@andrew.cmu.edu>
2025-07-16 02:27:11 +0800

    update project structure

--- api/user/controller.go
+++ api/user/controller.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"go-web-scaffold/pkg/logger"
 	"go-web-scaffold/pkg/models"
 	"go-web-scaffold/pkg/service"
 	"go-web-scaffold/pkg/utils"
@@ -104,4 +105,17 @@ func SaveUser(c *gin.Context) {
 		return
 	}
 	utils.Success(c)
+}
+
+func RegisterRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+	// 注册路由
+	r.GET("/ping", Ping)
+	r.GET("/user", GetByRange)               //查询参数
+	r.POST("/user/create", CreateUser)       //请求体参数
+	r.PUT("/user/update", UpdateUser)        //请求体参数
+	r.DELETE("/user/delete/:id", DeleteUser) // 路径参数
+	r.POST("/user/save/:id", SaveUser)       // 路径参数➕请求体参数
+	return r
 }
